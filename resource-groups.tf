befa5689e87aa225cd12016d316959f08911a777
Gordon Beeming <gordon@devstarops.com>
2022-08-01 19:29:41 +0200

    prevent_destroy

--- resource-groups.tf
+++ resource-groups.tf
@@ -2,14 +2,26 @@
 resource "azurerm_resource_group" "local" {
   name     = "devstarops-local-rg"
   location = var.deploy_region
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "azurerm_resource_group" "test" {
   name     = "devstarops-test-rg"
   location = var.deploy_region
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "azurerm_resource_group" "production" {
   name     = "devstarops-production-rg"
   location = var.deploy_region
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
--- storage.tf
+++ storage.tf
@@ -1,4 +1,8 @@
 data "azurerm_storage_account" "shared" {
   name                = "dsosharedstorage"
   resource_group_name = var.resource_group_name
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
